2b6c874fab6bb06c1d1e695876785867c45c70be
Edelweiss35 <45229901+Edelweiss35@users.noreply.github.com>
2019-05-07 18:52:44 -0700

    Update product-page.js

--- cypress/e2e/product-page.js
+++ cypress/e2e/product-page.js
@@ -1,12 +1,1 @@
-describe('add to cart', () => {
-  it('should allow me to add a product to the cart', () => {
-    cy.visit('/')
-      .waitForRouteChange()
-      .getByText(/multi-vibe/i)
-      .click({force: true})
-      .waitForRouteChange()
-      .getByText(/add to cart/i)
-      .click({force: true})
-      .getByText(/added to cart/i)
-  })
-})
+11
