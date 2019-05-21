d71725e0ac26037e82de97502ec8e5a64b46d8bf
Edelweiss35 <45229901+Edelweiss35@users.noreply.github.com>
2019-05-21 14:22:54 -0700

    Update index.js

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -12,11 +12,3 @@
 // You can read more here:
 // https://on.cypress.io/configuration
 // ***********************************************************
-
-// Import commands.js using ES2015 syntax:
-import 'cypress-testing-library/add-commands'
-import 'gatsby-cypress/commands'
-import './commands'
-
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
