8b3b5fa44ecd11be6a138731ead44e68c12ed58a
Edelweiss35 <45229901+Edelweiss35@users.noreply.github.com>
2019-05-28 19:13:39 -0700

    Update AuthProvider.js

--- src/components/Context/AuthProvider.js
+++ src/components/Context/AuthProvider.js
@@ -1,5 +1,16 @@
 
+import React, {useState, useEffect} from 'react'
+import AuthContext from './AuthContext'
 
+const AuthProvider = ({children}) => {
+  const [token, setToken] = useState(null)
+
+  const updateToken = () => setToken(localStorage.getItem('customerToken'))
+
+  const signOut = () => {
+    localStorage.removeItem('customerToken')
+    setToken('')
+  }
   useEffect(() => {
     const token = localStorage.getItem('customerToken')
     setToken(token)
