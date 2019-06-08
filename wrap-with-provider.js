a7ee7c2aa43b3ba0405ec4f839c77e8fc7515dde
Edelweiss35 <45229901+Edelweiss35@users.noreply.github.com>
2019-06-08 13:51:41 -0700

    Update wrap-with-provider.js

--- wrap-with-provider.js
+++ wrap-with-provider.js
@@ -2,9 +2,4 @@ import React from 'react'
 import AuthProvider from './src/components/Context/AuthProvider'
 import CartProvider from './src/components/Context/CartProvider'
 
-// eslint-disable-next-line import/prefer-default-export
-export default ({ element }) => (
-  <AuthProvider>
-    <CartProvider>{element}</CartProvider>
-  </AuthProvider>
-)
+
